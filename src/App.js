fbd2a1ae3f7fa84270b0f0d1d6644287ccf8caeb
123bhagesh <123bhagesh@gmail.com>
2023-06-03 14:24:14 +0530

    top component responsive completed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { Button } from "@mui/base";
 import { data } from "./Data/data";
 import { Box, Container, Typography } from "@mui/material";
+import { makeStyles, useTheme } from "@mui/styles";
 
 function App() {
   const [play, setPlay] = useState(false);
@@ -28,59 +29,78 @@ function App() {
     setPlay(!play);
   };
   useEffect(() => {
-    setNewData(data[index])
+    setNewData(data[index]);
     let timer;
     if (play) {
       timer = setInterval(handleNext, 4000);
     }
     return () => clearInterval(timer);
+  }, [handleNext, data, index]);
+  // console.log("NEWData", newData);
 
-  }, [handleNext,data,index]);
-
-  console.log("NEWData", newData);
   return (
     <div className="App">
       <Box
         sx={{
-          width: "90%",
+          width: "94%",
           margin: "auto",
           display: "flex",
+          flexDirection: { xs: "column", sm: "column", md: "row", lg: "row" },
           justifyContent: "space-between",
           columnGap: "20px",
           mt: "30px",
         }}
       >
-        <Box sx={{ width: "70%", height: "520px", border: "1px solid black" }}>
-          <img
-            style={{ width: "96%", height: "500px", borderRadius: "26px" }}
-            src={newData.img}
-          />
+        <Box
+          sx={{
+            width: { xs: "column", sm: "100%", md: "70%", lg: "70%" },
+            height: { xs: "100%", sm: "100%", md: "100%", lg: "520px" },
+            border: "1px solid red",
+          }}
+        >
+          <img className="image" src={newData.img} />
         </Box>
         <Box
           sx={{
-            width: "35%",
-            height: "520px",
+            width: { sm: "100%", md: "35%", lg: "35%" },
+            height: { sm: "100%", md: "505px", lg: "520px" },
             border: "1px solid black",
             textAlign: "left",
             color: "#545454",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            // mt: { xs: "14px"},
           }}
         >
           <Typography variant="h4">{newData.title}</Typography>
-          <Typography sx={{ mt: "16px" }}>{newData.description}</Typography>
+          <Typography
+            sx={{
+              mt: "16px",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              fontSize: "15px",
+              // display: "-webkit-box",
+            }}
+          >
+            {newData.description}
+          </Typography>
         </Box>
       </Box>
+      {/* ----------------------------------------------------------------------- */}
       <Box
         sx={{
           width: "90%",
           margin: "auto",
           display: "flex",
-          justifyContent: "space-between",
+          flexDirection: { xs: "column", sm: "column", md: "column", lg: "row" },
+          justifyContent:"center" ,
           columnGap: "20px",
+          margin:"auto"
         }}
       >
         <Box
           sx={{
-            width: "70%",
+            width: { xs: "column", sm: "100%", md: "50%", lg: "70%" },
             height: "120px",
             columnGap: "3px",
             display: "flex",
@@ -90,7 +110,11 @@ function App() {
           }}
         >
           <ArrowLeftIcon
-            style={{ width: "60px", height: "90px",cursor:"pointer" }}
+            style={{
+              width:"60px",
+              height: "90px",
+              cursor: "pointer",
+            }}
             onClick={handlePrev}
           />
 
@@ -107,32 +131,36 @@ function App() {
             ))}
           </Box>
           <ArrowRightIcon
-            style={{ width: "60px", height: "90px",cursor:"pointer" }}
+            style={{ width: "60px", height: "90px", cursor: "pointer" }}
             onClick={handleNext}
           />
         </Box>
-        <Box sx={{ width: "35%", height: "120px", border: "1px solid black",alignItems:"center",}}>
-            <Button
-              onClick={handleStart}
-              style={{
-                border: "none",
-                backgroundColor: "white",
-                cursor: "pointer",
-              }}
-            >
-              {play ?
-               (
-                <PauseCircleFilledIcon
-                  style={{ color: "#25beda", width: "90px", height: "90px" }}
-                />
-              )
-              :
-              (
-                <PlayCircleIcon
-                  style={{ color: "#25beda", width: "90px", height: "90px" }}
-                />
-              )}
-            </Button>
+        <Box
+          sx={{
+            width: { sm: "100%", md: "35%", lg: "35%" },
+            height: "120px",
+            border: "1px solid black",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            onClick={handleStart}
+            style={{
+              border: "none",
+              backgroundColor: "white",
+              cursor: "pointer",
+            }}
+          >
+            {play ? (
+              <PauseCircleFilledIcon
+                style={{ color: "#25beda", width: "90px", height: "90px" }}
+              />
+            ) : (
+              <PlayCircleIcon
+                style={{ color: "#25beda", width: "90px", height: "90px" }}
+              />
+            )}
+          </Button>
         </Box>
       </Box>
     </div>
